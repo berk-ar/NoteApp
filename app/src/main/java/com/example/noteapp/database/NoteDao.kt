dec73ca9b4d011aca5c9aa1e2e68807941d2b8c2
Berk AR <berk.ar12@yandex.com>
2023-05-30 23:25:47 +0300

    db class and dao added

---
+++ app/src/main/java/com/example/noteapp/database/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.noteapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.noteapp.models.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun  insert (note : Note)
+
+    @Delete
+    suspend fun  delete(note : Note)
+
+    @Query("SELECT * FROM note_table order by id ASC")
+    fun tumNotlar() : LiveData<List<Note>>
+
+    @Update
+    suspend fun  update(id : Int?, title : String?, note : String)
+}
