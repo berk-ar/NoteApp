0dd37285a0168ebfcea9a5f465cdadea96f6a7bd
Berk AR <berk.ar12@yandex.com>
2023-06-04 23:25:20 +0300

    adapter updated

--- app/src/main/java/com/example/noteapp/adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/noteapp/adapter/NotesAdapter.kt
@@ -1,25 +1,26 @@
 package com.example.noteapp.adapter
 
 import android.content.Context
+import android.provider.Settings
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.R
 import com.example.noteapp.models.Note
+import kotlin.random.Random
 
-class NotesAdapter(private val context : Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+class NotesAdapter(private val context : Context, val listener: NotesClickListener) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
-    private val notesList : ArrayList<Note>()
-    private val fullList : ArrayList<Note>()
-
-    inner class NoteViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-
-        val notes_layout = itemView.findViewById<CardView>(R.id.)
-
-    }
+    private val notesList = ArrayList<Note>()
+    private val fullList = ArrayList<Note>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        TODO("Not yet implemented")
+        return NoteViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.note_items,parent,false)
+        )
     }
 
     override fun getItemCount(): Int {
@@ -27,7 +28,82 @@ class NotesAdapter(private val context : Context) : RecyclerView.Adapter<NotesAd
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentNote = notesList[position]
+        holder.title.text = currentNote.title
+        holder.title.isSelected = true
+
+        holder.notes.text = currentNote.note
+        holder.date.text = currentNote.date
+        holder.date.isSelected = true
+
+        holder.notes_layout.setCardBackgroundColor(holder.itemView.resources.getColor(RandomColor(),null))
+
+        holder.notes_layout.setOnClickListener {
+
+            listener.onItemClicked(notesList[holder.adapterPosition])
+
+        }
+
+        holder.notes_layout.setOnLongClickListener {
+            listener.onLongItemClicked(notesList[holder.adapterPosition], holder.notes_layout)
+            true
+        }
+    }
+
+    fun updateList(newList : List<Note>){
+
+        fullList.clear()
+        fullList.addAll(newList)
+
+        notesList.clear()
+        notesList.addAll(fullList)
+        notifyDataSetChanged()
+
+    }
+
+    fun filterList(search : String){
+
+        notesList.clear()
+
+        for (item in fullList){
+
+            if (item.title?.lowercase()?.contains(search.lowercase()) == true ||
+                item.note?.lowercase()?.contains(search.lowercase()) == true )
+
+                notesList.add(item)
+        }
+
+        notifyDataSetChanged()
+
+    }
+
+    fun RandomColor() : Int{
+        val list = ArrayList<Int>()
+        list.add(R.color.NoteColor1)
+        list.add(R.color.NoteColor2)
+        list.add(R.color.NoteColor3)
+        list.add(R.color.NoteColor4)
+        list.add(R.color.NoteColor5)
+        list.add(R.color.NoteColor6)
+
+        val seed = System.currentTimeMillis().toInt()
+        val randomIndex = Random(seed).nextInt(list.size)
+        return list[randomIndex]
+        //rastgele renk seçimi
+    }
+
+    inner class NoteViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        val notes_layout = itemView.findViewById<CardView>(R.id.card_layout)
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val notes = itemView.findViewById<TextView>(R.id.tv_note)
+        val date = itemView.findViewById<TextView>(R.id.tv_date)
+
+    }
+
+    interface NotesClickListener{
+        fun onItemClicked(note:Note)
+        fun onLongItemClicked(note:Note,cardView: CardView)
     }
 
 }
